64a47f7a070c0f4b92b03ba366ee8f23b277196b
GitHubAci <60788672+GitHubAci@users.noreply.github.com>
2020-03-10 10:43:57 +0100

    Add files via upload

---
+++ GUI_WPF/Views/CreateCampaignView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using BusinessEntities.Enums;
+using GUI_WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI_WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for CreateCampaign.xaml
+    /// </summary>
+    public partial class CreateCampaignView : UserControl
+    {
+        public CreateCampaignView()
+        {
+            InitializeComponent();
+            ComboBox.ItemsSource = Enum.GetValues(typeof(Education));
+            
+        }
+
+        
+    }
+}
